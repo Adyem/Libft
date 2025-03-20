683ec92bdae8ac0229f07c036f00209b9413ef94
Bastian Vangenechten <bvangene@a1-r3-p3.s19.be>
2025-03-20 16:19:35 +0100

    cleaning up the cross compatible windows hpp file

--- Windows/windows_file.hpp
+++ Windows/windows_file.hpp
@@ -10,33 +10,13 @@ typedef SSIZE_T ssize_t;
 # define DEBUG 0
 #endif
 
-#ifndef O_RDONLY
 # define O_RDONLY    0x0000
-#endif
-
-#ifndef O_WRONLY
 # define O_WRONLY    0x0001
-#endif
-
-#ifndef O_RDWR
 # define O_RDWR      0x0002
-#endif
-
-#ifndef O_CREAT
 # define O_CREAT     0x0100
-#endif
-
-#ifndef O_EXCL
 # define O_EXCL      0x0200
-#endif
-
-#ifndef O_TRUNC
 # define O_TRUNC     0x0400
-#endif
-
-#ifndef O_APPEND
 # define O_APPEND    0x0800
-#endif
 
 int		ft_open(const char *pathname, int flags, int mode); 
 ssize_t	ft_read(int fd, void *buf, unsigned int count);
