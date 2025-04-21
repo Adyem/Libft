6ce2e3c1fddb46bea04f46563ba2df2eaee1fb38
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2025-04-21 20:25:27 +0200

    adding a header to opendir

--- Libft/ft_opendir.cpp
+++ Libft/ft_opendir.cpp
@@ -4,6 +4,7 @@
 #include "../CMA/CMA.hpp"
 #include "../Linux/linux_file.hpp"
 #include "../CPP_class/nullptr.hpp"
+#include "../Windows/windows_file.hpp"
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
