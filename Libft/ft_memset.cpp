6cd184754bb31f1202cafa1ccfe3687db3cef3d8
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2025-04-29 19:49:56 +0200

    removing duplicate function

--- Libft/ft_memset.cpp
+++ Libft/ft_memset.cpp
@@ -14,20 +14,3 @@ void *ft_memset(void *destination, int value, size_t number_of_bytes)
     }
     return (destination);
 }
-
-void* ft_memcpy(void* destination, const void* source, size_t num)
-{
-    if (destination == ft_nullptr && source == ft_nullptr)
-        return ft_nullptr;
-
-    char* destPtr = static_cast<char*>(destination);
-    const char* srcPtr = static_cast<const char*>(source);
-    size_t index = 0;
-    while (index < num)
-    {
-        destPtr[index] = srcPtr[index];
-        ++index;
-    }
-    return destination;
-}
-
