7f77c4ca58412da17a4f1ee9bfeed55c33a1fdde
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2025-06-30 21:09:00 +0200

    using internal functions rather then main library ones

--- JSon/json_utils.cpp
+++ JSon/json_utils.cpp
@@ -1,13 +1,14 @@
 #include <cstring>
 #include "json.hpp"
 #include "../CPP_class/nullptr.hpp"
+#include "../Libft/libft.hpp"
 
 json_group *json_find_group(json_group *head, const char *name)
 {
     json_group *current = head;
     while (current)
     {
-        if (current->name && std::strcmp(current->name, name) == 0)
+        if (current->name && ft_strcmp(current->name, name) == 0)
             return (current);
         current = current->next;
     }
@@ -21,7 +22,7 @@ json_item *json_find_item(json_group *group, const char *key)
     json_item *current = group->items;
     while (current)
     {
-        if (current->key && std::strcmp(current->key, key) == 0)
+        if (current->key && ft_strcmp(current->key, key) == 0)
             return (current);
         current = current->next;
     }
