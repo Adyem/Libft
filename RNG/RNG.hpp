faecd545cc83d26091bbce3a9a3506d589de54e0
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2025-08-08 11:37:57 +0200

    moving some functions to internal libraries

--- RNG/RNG.hpp
+++ RNG/RNG.hpp
@@ -1,25 +1,12 @@
-#ifndef DICE_ROLL_HPP
-# define DICE_ROLL_HPP
+#ifndef RNG_HPP
+# define RNG_HPP
 
 #ifndef DEBUG
 # define DEBUG 0
 #endif
 
-#include "../CPP_class/nullptr.hpp"
 #include <ctime>
 #include <cstdlib>
-
-extern bool g_srand_init;
-
-inline __attribute__((always_inline)) void ft_init_srand(void)
-{
-    if (g_srand_init == false)
-    {
-        srand(static_cast<unsigned>(time(ft_nullptr)));
-        g_srand_init = true;
-    }
-	return ;
-}
 
 int ft_random_int(void);
 int ft_dice_roll(int number, int faces);
