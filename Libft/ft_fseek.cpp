e77039f926f955906a8268356ac4328515661a5c
Adyem <136982734+Adyem@users.noreply.github.com>
2025-08-15 23:38:44 +0200

    Add ft_fseek and ft_ftell

---
+++ Libft/ft_fseek.cpp
@@ -0,0 +1,11 @@
+#include "libft.hpp"
+#include "../CPP_class/nullptr.hpp"
+#include <cstdio>
+
+int ft_fseek(FILE *stream, long offset, int origin)
+{
+    if (stream == ft_nullptr)
+        return (-1);
+    return (std::fseek(stream, offset, origin));
+}
+
