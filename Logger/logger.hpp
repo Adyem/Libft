b78c86ab1793038c49acaf47feeb9078737b6035
Adyem <136982734+Adyem@users.noreply.github.com>
2025-08-17 14:41:12 +0200

    Add debug log level

--- Logger/logger.hpp
+++ Logger/logger.hpp
@@ -4,5 +4,6 @@
 void ft_log_info(int fd, const char *message);
 void ft_log_warn(int fd, const char *message);
 void ft_log_error(int fd, const char *message);
+void ft_log_debug(int fd, const char *message);
 
 #endif
