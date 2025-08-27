efd489ebef2f9f9afc2f7a33f2913947aa03ca68
Adyem <136982734+Adyem@users.noreply.github.com>
2025-08-27 20:13:01 +0200

    Add TLS promise wrappers to API

--- API/api_promise.hpp
+++ API/api_promise.hpp
@@ -24,4 +24,24 @@ public:
                  int timeout = 60000);
 };
 
+class api_tls_promise : public ft_promise<json_group*>
+{
+public:
+    bool request(const char *host, uint16_t port,
+                 const char *method, const char *path,
+                 json_group *payload = NULL,
+                 const char *headers = NULL, int *status = NULL,
+                 int timeout = 60000);
+};
+
+class api_tls_string_promise : public ft_promise<char*>
+{
+public:
+    bool request(const char *host, uint16_t port,
+                 const char *method, const char *path,
+                 json_group *payload = NULL,
+                 const char *headers = NULL, int *status = NULL,
+                 int timeout = 60000);
+};
+
 #endif
