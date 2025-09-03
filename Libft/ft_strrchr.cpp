bc98543b95f990d8e7b0d9cd45e4dcd37587c36d
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-03 13:46:14 +0200

    Fix strchr implementation and add test

--- Libft/ft_strrchr.cpp
+++ Libft/ft_strrchr.cpp
@@ -3,14 +3,14 @@
 
 char    *ft_strrchr(const char *string, int char_to_find)
 {
-    int     string_length;
-
-    string_length = ft_strlen(string);
+    if (!string)
+        return (ft_nullptr);
+    int string_length = ft_strlen(string);
     while (string_length > 0)
     {
         if (string[string_length] == static_cast<char>(char_to_find))
             return (const_cast<char*>(string) + string_length);
-        string_length--;
+        --string_length;
     }
     if (string[string_length] == static_cast<char>(char_to_find))
         return (const_cast<char*>(string) + string_length);
