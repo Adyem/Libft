92d00dffa6d83492c7f867cd8aa165d21d78739f
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-10 20:47:13 +0200

    Fix Printf internal include for ft_file

--- Printf/printf.hpp
+++ Printf/printf.hpp
@@ -7,6 +7,7 @@
 
 int pf_printf(const char *format, ...) __attribute__((format(printf, 1, 2), hot));
 int pf_printf_fd(int fd, const char *format, ...) __attribute__((format(printf, 2, 3), hot));
+int pf_snprintf(char *string, size_t size, const char *format, ...) __attribute__((format(printf, 3, 4), hot));
 int ft_vfprintf(FILE *stream, const char *format, va_list args);
 int ft_fprintf(FILE *stream, const char *format, ...) __attribute__((format(printf, 2, 3), hot));
 
