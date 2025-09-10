c73db7da57e41dcdd844e0857fa9c03972c5be8c
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-10 22:08:40 +0200

    Add AES encryption support

--- Encryption/basic_encryption.hpp
+++ Encryption/basic_encryption.hpp
@@ -1,8 +1,18 @@
 #ifndef BASIC_ENCRYPTION_HPP
 #define BASIC_ENCRYPTION_HPP
 
+#include "aes.hpp"
+
 int            be_saveGame(const char *filename, const char *data, const char *key);
 char        **be_DecryptData(char **data, const char *key);
 const char    *be_getEncryptionKey();
+
+/*
+Usage:
+    unsigned char block[16] = {0};
+    unsigned char key[16] = {0};
+    aes_encrypt(block, key);
+    aes_decrypt(block, key);
+*/
 
 #endif
