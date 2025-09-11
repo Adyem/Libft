15e8ba0b76235d0ef4d05f5aa842163efea12820
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-11 20:54:10 +0200

    Add basic RSA encryption

--- Encryption/basic_encryption.hpp
+++ Encryption/basic_encryption.hpp
@@ -2,6 +2,7 @@
 #define BASIC_ENCRYPTION_HPP
 
 #include "aes.hpp"
+#include "rsa.hpp"
 
 int            be_saveGame(const char *filename, const char *data, const char *key);
 char        **be_DecryptData(char **data, const char *key);
@@ -13,6 +14,13 @@ Usage:
     unsigned char key[16] = {0};
     aes_encrypt(block, key);
     aes_decrypt(block, key);
+
+    uint64_t public_key;
+    uint64_t private_key;
+    uint64_t modulus;
+    rsa_generate_key_pair(&public_key, &private_key, &modulus, 32);
+    uint64_t encrypted = rsa_encrypt(42, public_key, modulus);
+    uint64_t decrypted = rsa_decrypt(encrypted, private_key, modulus);
 */
 
 #endif
