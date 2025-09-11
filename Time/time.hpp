53374d9b6bac14b7ee141f31dd36c3a36129564c
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-11 08:04:55 +0200

    Add monotonic time helper

--- Time/time.hpp
+++ Time/time.hpp
@@ -5,6 +5,7 @@
 
 time_t  time_now(void);
 long    time_now_ms(void);
+long    time_monotonic(void);
 void    time_local(time_t time_value, struct tm *out);
 void    time_sleep(unsigned int seconds);
 void    time_sleep_ms(unsigned int milliseconds);
