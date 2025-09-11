33d95aa796d165ef6c22215832c0fb62d5b3ca9f
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-11 09:46:25 +0200

    Add configurable thread safety to CMA

--- CMA/cma_set_alloc_limit.cpp
+++ CMA/cma_set_alloc_limit.cpp
@@ -4,6 +4,10 @@
 
 void    cma_set_alloc_limit(std::size_t limit)
 {
+    if (g_cma_thread_safe)
+        g_malloc_mutex.lock(THREAD_ID);
     g_cma_alloc_limit = limit;
+    if (g_cma_thread_safe)
+        g_malloc_mutex.unlock(THREAD_ID);
     return ;
 }
