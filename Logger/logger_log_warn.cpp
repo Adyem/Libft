52aa9ec21a4e3c2a0d9b97097362941742556545
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-11 11:18:56 +0200

    feat: add async logger

--- Logger/logger_log_warn.cpp
+++ Logger/logger_log_warn.cpp
@@ -4,6 +4,9 @@ void ft_log_warn(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    ft_log_vwrite(LOG_LEVEL_WARN, fmt, args);
+    if (g_async_running)
+        ft_log_enqueue(LOG_LEVEL_WARN, fmt, args);
+    else
+        ft_log_vwrite(LOG_LEVEL_WARN, fmt, args);
     va_end(args);
 }
