1102153c5e697874d444aab66a96009229ceb367
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-12 16:49:44 +0200

    Refactor linear algebra sources and fix base64 warning

---
+++ Math/linear_algebra_vector2.cpp
@@ -0,0 +1,65 @@
+#include "linear_algebra.hpp"
+#include "math.hpp"
+#include "../Errno/errno.hpp"
+
+double  vector2::get_x() const
+{
+    return (this->_x);
+}
+
+double  vector2::get_y() const
+{
+    return (this->_y);
+}
+
+double  vector2::dot(const vector2 &other) const
+{
+    this->set_error(ER_SUCCESS);
+    return (this->_x * other._x + this->_y * other._y);
+}
+
+double  vector2::length() const
+{
+    double result;
+
+    result = this->_x * this->_x + this->_y * this->_y;
+    this->set_error(ER_SUCCESS);
+    return (math_sqrt(result));
+}
+
+vector2 vector2::normalize() const
+{
+    double len;
+    double epsilon;
+    vector2 result;
+
+    len = this->length();
+    epsilon = 0.0000001;
+    if (math_absdiff(len, 0.0) <= epsilon)
+    {
+        result.set_error(FT_EINVAL);
+        return (result);
+    }
+    result._x = this->_x / len;
+    result._y = this->_y / len;
+    result.set_error(ER_SUCCESS);
+    return (result);
+}
+
+void    vector2::set_error(int error_code) const
+{
+    ft_errno = error_code;
+    this->_error_code = error_code;
+    return ;
+}
+
+int     vector2::get_error() const
+{
+    return (this->_error_code);
+}
+
+const char  *vector2::get_error_str() const
+{
+    return (ft_strerror(this->_error_code));
+}
+
---
+++ Math/linear_algebra_vector4.cpp
@@ -0,0 +1,77 @@
+#include "linear_algebra.hpp"
+#include "math.hpp"
+#include "../Errno/errno.hpp"
+
+double  vector4::get_x() const
+{
+    return (this->_x);
+}
+
+double  vector4::get_y() const
+{
+    return (this->_y);
+}
+
+double  vector4::get_z() const
+{
+    return (this->_z);
+}
+
+double  vector4::get_w() const
+{
+    return (this->_w);
+}
+
+double  vector4::dot(const vector4 &other) const
+{
+    this->set_error(ER_SUCCESS);
+    return (this->_x * other._x + this->_y * other._y + this->_z * other._z + this->_w * other._w);
+}
+
+double  vector4::length() const
+{
+    double result;
+
+    result = this->_x * this->_x + this->_y * this->_y + this->_z * this->_z + this->_w * this->_w;
+    this->set_error(ER_SUCCESS);
+    return (math_sqrt(result));
+}
+
+vector4 vector4::normalize() const
+{
+    double len;
+    double epsilon;
+    vector4 result;
+
+    len = this->length();
+    epsilon = 0.0000001;
+    if (math_absdiff(len, 0.0) <= epsilon)
+    {
+        result.set_error(FT_EINVAL);
+        return (result);
+    }
+    result._x = this->_x / len;
+    result._y = this->_y / len;
+    result._z = this->_z / len;
+    result._w = this->_w / len;
+    result.set_error(ER_SUCCESS);
+    return (result);
+}
+
+void    vector4::set_error(int error_code) const
+{
+    ft_errno = error_code;
+    this->_error_code = error_code;
+    return ;
+}
+
+int     vector4::get_error() const
+{
+    return (this->_error_code);
+}
+
+const char  *vector4::get_error_str() const
+{
+    return (ft_strerror(this->_error_code));
+}
+
