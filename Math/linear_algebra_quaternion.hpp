a9bbdc79aff59c5e123d9d372988d9b39a70fd60
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-12 22:37:26 +0200

    Add quaternion class with basic operations and tests

---
+++ Math/linear_algebra_quaternion.hpp
@@ -0,0 +1,32 @@
+#ifndef LINEAR_ALGEBRA_QUATERNION_HPP
+# define LINEAR_ALGEBRA_QUATERNION_HPP
+
+class quaternion
+{
+    private:
+        double _w;
+        double _x;
+        double _y;
+        double _z;
+        mutable int _error_code;
+
+        void    set_error(int error_code) const;
+
+    public:
+        quaternion();
+        quaternion(double w, double x, double y, double z);
+        ~quaternion();
+        double      get_w() const;
+        double      get_x() const;
+        double      get_y() const;
+        double      get_z() const;
+        quaternion  add(const quaternion &other) const;
+        quaternion  multiply(const quaternion &other) const;
+        quaternion  conjugate() const;
+        double      length() const;
+        quaternion  normalize() const;
+        int         get_error() const;
+        const char  *get_error_str() const;
+};
+
+#endif
