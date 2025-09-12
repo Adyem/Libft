785cc38ea0b1bdaea877c6db6e55bb7e2c218523
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-12 16:03:38 +0200

    docs: consolidate RNG secure usage

--- RNG/rng.hpp
+++ RNG/rng.hpp
@@ -7,6 +7,8 @@
 
 #include <ctime>
 #include <cstdlib>
+#include <cstddef>
+#include <cstdint>
 #include "../CPP_class/class_nullptr.hpp"
 
 int ft_random_int(void);
@@ -16,5 +18,7 @@ float ft_random_normal(void);
 float ft_random_exponential(float lambda_value);
 int ft_random_poisson(double lambda_value);
 int ft_random_seed(const char *seed_str = ft_nullptr);
+int rng_secure_bytes(unsigned char *buffer, size_t length);
+uint32_t ft_random_uint32(void);
 
 #endif
