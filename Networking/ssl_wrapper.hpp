86bb598f64c8d07e28f9d4c09c08ab320348c2be
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-22 21:02:16 +0200

    Refactor networking tests to use harness and drop send stubs

--- Networking/ssl_wrapper.hpp
+++ Networking/ssl_wrapper.hpp
@@ -12,6 +12,5 @@ typedef SSIZE_T ssize_t;
 
 ssize_t nw_ssl_write(SSL *ssl, const void *buf, size_t len);
 ssize_t nw_ssl_read(SSL *ssl, void *buf, size_t len);
-void nw_set_ssl_write_stub(ssize_t (*ssl_write_stub)(SSL *ssl, const void *buffer, size_t length));
 
 #endif
