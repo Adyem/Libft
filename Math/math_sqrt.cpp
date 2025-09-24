f8169442e3c0ef01e5c606f0e1d16a2b0106ecb7
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-24 23:18:10 +0200

    Fix zero guard in math_pow

--- Math/math_sqrt.cpp
+++ Math/math_sqrt.cpp
@@ -1,21 +1,40 @@
 #include "math.hpp"
+#include "../Errno/errno.hpp"
 
 double math_sqrt(double number)
 {
     double guess;
     double next_guess;
+    double difference;
+    int    iteration_count;
+    int    max_iterations;
 
+    if (math_isnan(number))
+        return (math_nan());
     if (number < 0)
+    {
+        ft_errno = FT_EINVAL;
         return (-1.0);
+    }
     if (math_fabs(number) < 1e-12)
         return (0.0);
     guess = number;
-    while (1)
+    iteration_count = 0;
+    max_iterations = 1000;
+    while (iteration_count < max_iterations)
     {
         next_guess = 0.5 * (guess + number / guess);
-        if (math_fabs(next_guess - guess) < 0.000001)
-            break;
+        if (math_isnan(next_guess))
+        {
+            ft_errno = FT_ERANGE;
+            return (math_nan());
+        }
+        difference = math_fabs(next_guess - guess);
+        if (difference < 0.000001)
+            return (next_guess);
         guess = next_guess;
+        iteration_count += 1;
     }
-    return (next_guess);
+    ft_errno = FT_ERANGE;
+    return (math_nan());
 }
