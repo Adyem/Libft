cb71631101dd57b3e93e6125e1ad85293b1d8209
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-24 15:52:55 +0200

    Fix allocator split null handling and harden ft_strlcat

--- Libft/libft_strlcat.cpp
+++ Libft/libft_strlcat.cpp
@@ -1,10 +1,18 @@
 #include "libft.hpp"
+#include "../CPP_class/class_nullptr.hpp"
+#include "../Errno/errno.hpp"
 
 size_t    ft_strlcat(char *destination, const char *source, size_t bufferSize)
 {
     size_t    destLength = 0;
     size_t    sourceIndex = 0;
 
+    ft_errno = ER_SUCCESS;
+    if (destination == ft_nullptr || source == ft_nullptr)
+    {
+        ft_errno = FT_EINVAL;
+        return (0);
+    }
     while (destination[destLength] && destLength < bufferSize)
         destLength++;
     while (source[sourceIndex] && (destLength + sourceIndex + 1) < bufferSize)
