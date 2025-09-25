7ca9eae8979f9914d3fea5a7c6ab86cb99989271
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-25 21:09:03 +0200

    Add portable time retrieval helper

--- Libft/libft_fclose.cpp
+++ Libft/libft_fclose.cpp
@@ -1,6 +1,7 @@
 #include "libft.hpp"
 #include "../CPP_class/class_nullptr.hpp"
 #include "../Errno/errno.hpp"
+#include <cerrno>
 #include <cstdio>
 
 int ft_fclose(FILE *stream)
@@ -15,7 +16,13 @@ int ft_fclose(FILE *stream)
     close_result = std::fclose(stream);
     if (close_result != 0)
     {
-        ft_errno = FILE_INVALID_FD;
+        int close_error;
+
+        close_error = errno;
+        if (close_error != 0)
+            ft_errno = close_error + ERRNO_OFFSET;
+        else
+            ft_errno = FILE_INVALID_FD;
         return (EOF);
     }
     ft_errno = ER_SUCCESS;
