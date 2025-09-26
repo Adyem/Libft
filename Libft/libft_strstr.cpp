998c84b1bb932dc910bca4357c5e0ec7b843b875
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-26 21:31:25 +0200

    Ensure strchr-family APIs manage errno

--- Libft/libft_strstr.cpp
+++ Libft/libft_strstr.cpp
@@ -1,12 +1,19 @@
 #include "libft.hpp"
 #include "../CPP_class/class_nullptr.hpp"
+#include "../Errno/errno.hpp"
 
 char    *ft_strstr(const char *haystack, const char *needle)
 {
     size_t  haystack_length;
 
-    if (!haystack || !needle)
+    ft_errno = ER_SUCCESS;
+    if (haystack == ft_nullptr || needle == ft_nullptr)
+    {
+        ft_errno = FT_EINVAL;
         return (ft_nullptr);
-    haystack_length = ft_strlen(haystack);
+    }
+    haystack_length = ft_strlen_size_t(haystack);
+    if (ft_errno != ER_SUCCESS)
+        return (ft_nullptr);
     return (ft_strnstr(haystack, needle, haystack_length));
 }
--- Test/Test/test_strrchr.cpp
+++ Test/Test/test_strrchr.cpp
@@ -1,12 +1,15 @@
 #include "../../Libft/libft.hpp"
 #include "../../CPP_class/class_nullptr.hpp"
+#include "../../Errno/errno.hpp"
 #include "../../System_utils/test_runner.hpp"
 
 FT_TEST(test_strrchr_last, "ft_strrchr last occurrence")
 {
     const char *string = "banana";
 
+    ft_errno = FT_ERANGE;
     FT_ASSERT_EQ(string + 5, ft_strrchr(string, 'a'));
+    FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
     return (1);
 }
 
@@ -18,7 +21,9 @@ FT_TEST(test_strrchr_not_found, "ft_strrchr not found")
 
 FT_TEST(test_strrchr_null, "ft_strrchr nullptr")
 {
+    ft_errno = ER_SUCCESS;
     FT_ASSERT_EQ(ft_nullptr, ft_strrchr(ft_nullptr, 'a'));
+    FT_ASSERT_EQ(FT_EINVAL, ft_errno);
     return (1);
 }
 
