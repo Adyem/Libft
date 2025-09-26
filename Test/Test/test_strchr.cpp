83dbcd42ddb056e341ed119de1ecb3662b923fbe
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-26 21:31:25 +0200

    Ensure strchr-family APIs manage errno

--- Test/Test/test_strchr.cpp
+++ Test/Test/test_strchr.cpp
@@ -1,11 +1,14 @@
 #include "../../Libft/libft.hpp"
 #include "../../CPP_class/class_nullptr.hpp"
+#include "../../Errno/errno.hpp"
 #include "../../System_utils/test_runner.hpp"
 
 FT_TEST(test_strchr_basic, "ft_strchr basic")
 {
     const char *string = "hello";
+    ft_errno = FT_ERANGE;
     FT_ASSERT_EQ(string + 1, ft_strchr(string, 'e'));
+    FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
     return (1);
 }
 
@@ -17,7 +20,9 @@ FT_TEST(test_strchr_not_found, "ft_strchr not found")
 
 FT_TEST(test_strchr_null, "ft_strchr nullptr")
 {
+    ft_errno = ER_SUCCESS;
     FT_ASSERT_EQ(ft_nullptr, ft_strchr(ft_nullptr, 'a'));
+    FT_ASSERT_EQ(FT_EINVAL, ft_errno);
     return (1);
 }
 
