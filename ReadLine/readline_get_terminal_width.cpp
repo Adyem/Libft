04bf7a19830c18d3cb648889e806a87b0447e955
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-28 23:04:36 +0200

    Improve readline error handling

--- ReadLine/readline_get_terminal_width.cpp
+++ ReadLine/readline_get_terminal_width.cpp
@@ -1,7 +1,13 @@
 #include "readline_internal.hpp"
 #include "../Compatebility/compatebility_internal.hpp"
+#include "../Errno/errno.hpp"
 
 int rl_get_terminal_width(void)
 {
-    return (cmp_readline_terminal_width());
+    int width;
+
+    width = cmp_readline_terminal_width();
+    if (width == -1 && ft_errno == ER_SUCCESS)
+        ft_errno = FT_ETERM;
+    return (width);
 }
