67e25d8dcc96eaeb44144b54054dbbebd5ab3d52
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-29 09:39:11 +0200

    Preserve ft_errno across CMA alloc limit changes

--- CMA/cma_set_alloc_limit.cpp
+++ CMA/cma_set_alloc_limit.cpp
@@ -1,13 +1,30 @@
 #include <cstddef>
 #include "CMA.hpp"
 #include "cma_internal.hpp"
+#include "../Errno/errno.hpp"
 
 void    cma_set_alloc_limit(ft_size_t limit)
 {
+    int entry_errno;
+    int lock_result;
+    int current_errno;
+
+    entry_errno = ft_errno;
     if (g_cma_thread_safe)
-        g_malloc_mutex.lock(THREAD_ID);
+    {
+        lock_result = g_malloc_mutex.lock(THREAD_ID);
+        if (lock_result != FT_SUCCESS)
+            return ;
+        ft_errno = entry_errno;
+    }
     g_cma_alloc_limit = limit;
     if (g_cma_thread_safe)
-        g_malloc_mutex.unlock(THREAD_ID);
+    {
+        current_errno = ft_errno;
+        lock_result = g_malloc_mutex.unlock(THREAD_ID);
+        if (lock_result != FT_SUCCESS)
+            return ;
+        ft_errno = current_errno;
+    }
     return ;
 }
