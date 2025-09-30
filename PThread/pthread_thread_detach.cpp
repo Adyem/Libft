2e8291e8bbc9f37866b7f1f470875501dc2fec20
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-30 10:32:18 +0200

    Fix pthread errno propagation and add regression tests

--- PThread/pthread_thread_detach.cpp
+++ PThread/pthread_thread_detach.cpp
@@ -1,12 +1,17 @@
-#include <cerrno>
 #include <pthread.h>
 #include "pthread.hpp"
 #include "../Errno/errno.hpp"
 
 int pt_thread_detach(pthread_t thread)
 {
-        int return_value = pthread_detach(thread);
-        if (return_value != 0)
-                ft_errno = errno + ERRNO_OFFSET;
+    int return_value;
+
+    return_value = pthread_detach(thread);
+    if (return_value != 0)
+    {
+        ft_errno = return_value + ERRNO_OFFSET;
         return (return_value);
+    }
+    ft_errno = ER_SUCCESS;
+    return (return_value);
 }
