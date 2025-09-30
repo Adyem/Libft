86d4035149ca9a3c5382689c422cf82251afb69c
Adyem <136982734+Adyem@users.noreply.github.com>
2025-09-30 23:19:59 +0200

    Fix directory error reporting

--- File/file_opendir.cpp
+++ File/file_opendir.cpp
@@ -9,15 +9,29 @@ file_dir *file_opendir(const char *directory_path)
 
 file_dirent *file_readdir(file_dir *directory_stream)
 {
+    file_dirent *directory_entry;
+
     if (!directory_stream)
+    {
+        ft_errno = FT_EINVAL;
         return (ft_nullptr);
-    return (cmp_dir_read(directory_stream));
+    }
+    directory_entry = cmp_dir_read(directory_stream);
+    ft_errno = ER_SUCCESS;
+    return (directory_entry);
 }
 
 int file_closedir(file_dir *directory_stream)
 {
+    int close_result;
+
     if (!directory_stream)
+    {
+        ft_errno = FT_EINVAL;
         return (-1);
-    return (cmp_dir_close(directory_stream));
+    }
+    close_result = cmp_dir_close(directory_stream);
+    ft_errno = ER_SUCCESS;
+    return (close_result);
 }
 
