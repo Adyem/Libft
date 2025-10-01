d362143d1eaf431edd1e77d9de396b8153ce1793
Adyem <136982734+Adyem@users.noreply.github.com>
2025-10-01 09:29:32 +0200

    Fix time_now errno handling and add tests

--- Time/time_now.cpp
+++ Time/time_now.cpp
@@ -1,12 +1,25 @@
 #include "time.hpp"
 #include "../CPP_class/class_nullptr.hpp"
+#include "../Errno/errno.hpp"
 #include <ctime>
+#include <cerrno>
 
 t_time  time_now(void)
 {
     std::time_t standard_time;
+    int saved_errno;
 
     standard_time = std::time(ft_nullptr);
+    if (standard_time == static_cast<std::time_t>(-1))
+    {
+        saved_errno = errno;
+        if (saved_errno != 0)
+            ft_errno = saved_errno;
+        else
+            ft_errno = FT_ETERM;
+        return (static_cast<t_time>(-1));
+    }
+    ft_errno = ER_SUCCESS;
     return (static_cast<t_time>(standard_time));
 }
 
