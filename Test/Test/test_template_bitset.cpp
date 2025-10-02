b380a8f69b5cd7f9d1eecb5dbca0b954de6ec46b
Adyem <136982734+Adyem@users.noreply.github.com>
2025-10-02 13:49:09 +0200

    Add ft_deque tests

---
+++ Test/Test/test_template_bitset.cpp
@@ -0,0 +1,85 @@
+#include "../../Template/bitset.hpp"
+#include "../../Template/move.hpp"
+#include "../../Errno/errno.hpp"
+#include "../../System_utils/test_runner.hpp"
+
+FT_TEST(test_ft_bitset_basic_operations, "ft_bitset set, reset, flip, and test manage bit states")
+{
+    ft_bitset bitset_value(16);
+
+    FT_ASSERT_EQ(16UL, bitset_value.size());
+    FT_ASSERT_EQ(ER_SUCCESS, bitset_value.get_error());
+
+    bitset_value.set(3);
+    FT_ASSERT(bitset_value.test(3));
+    FT_ASSERT_EQ(ER_SUCCESS, bitset_value.get_error());
+
+    bitset_value.set(7);
+    FT_ASSERT(bitset_value.test(7));
+    FT_ASSERT_EQ(ER_SUCCESS, bitset_value.get_error());
+
+    bitset_value.reset(3);
+    FT_ASSERT_EQ(false, bitset_value.test(3));
+    FT_ASSERT(bitset_value.test(7));
+    FT_ASSERT_EQ(ER_SUCCESS, bitset_value.get_error());
+
+    bitset_value.flip(7);
+    FT_ASSERT_EQ(false, bitset_value.test(7));
+    FT_ASSERT_EQ(ER_SUCCESS, bitset_value.get_error());
+
+    bitset_value.set(0);
+    bitset_value.clear();
+    FT_ASSERT_EQ(false, bitset_value.test(0));
+    FT_ASSERT_EQ(ER_SUCCESS, bitset_value.get_error());
+    return (1);
+}
+
+FT_TEST(test_ft_bitset_out_of_range_errors, "ft_bitset reports errors when accessing out of range indices")
+{
+    ft_bitset bitset_value(8);
+
+    bitset_value.set(12);
+    FT_ASSERT_EQ(BITSET_OUT_OF_RANGE, bitset_value.get_error());
+
+    FT_ASSERT_EQ(false, bitset_value.test(9));
+    FT_ASSERT_EQ(BITSET_OUT_OF_RANGE, bitset_value.get_error());
+
+    bitset_value.reset(15);
+    FT_ASSERT_EQ(BITSET_OUT_OF_RANGE, bitset_value.get_error());
+
+    bitset_value.set(4);
+    FT_ASSERT(bitset_value.test(4));
+    FT_ASSERT_EQ(ER_SUCCESS, bitset_value.get_error());
+    return (1);
+}
+
+FT_TEST(test_ft_bitset_move_transfers_storage, "ft_bitset move operations transfer storage without data loss")
+{
+    ft_bitset source_bitset(12);
+
+    source_bitset.set(1);
+    source_bitset.set(8);
+    FT_ASSERT(source_bitset.test(1));
+    FT_ASSERT(source_bitset.test(8));
+
+    ft_bitset moved_bitset(ft_move(source_bitset));
+
+    FT_ASSERT_EQ(12UL, moved_bitset.size());
+    FT_ASSERT(moved_bitset.test(1));
+    FT_ASSERT(moved_bitset.test(8));
+    FT_ASSERT_EQ(ER_SUCCESS, moved_bitset.get_error());
+
+    FT_ASSERT_EQ(0UL, source_bitset.size());
+    FT_ASSERT_EQ(ER_SUCCESS, source_bitset.get_error());
+
+    ft_bitset assigned_bitset(3);
+    assigned_bitset = ft_move(moved_bitset);
+
+    FT_ASSERT_EQ(12UL, assigned_bitset.size());
+    FT_ASSERT(assigned_bitset.test(1));
+    FT_ASSERT(assigned_bitset.test(8));
+    FT_ASSERT_EQ(ER_SUCCESS, assigned_bitset.get_error());
+    FT_ASSERT_EQ(0UL, moved_bitset.size());
+    FT_ASSERT_EQ(ER_SUCCESS, moved_bitset.get_error());
+    return (1);
+}
---
+++ Test/Test/test_template_deque.cpp
@@ -0,0 +1,121 @@
+#include "../../Template/deque.hpp"
+#include "../../Template/move.hpp"
+#include "../../CMA/CMA.hpp"
+#include "../../Errno/errno.hpp"
+#include "../../System_utils/test_runner.hpp"
+
+FT_TEST(test_ft_deque_push_pop_order, "ft_deque preserves order across push and pop operations")
+{
+    ft_deque<int> deque_instance;
+
+    deque_instance.push_back(5);
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    deque_instance.push_front(3);
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    deque_instance.push_back(7);
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+
+    FT_ASSERT_EQ(false, deque_instance.empty());
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    FT_ASSERT_EQ(3UL, deque_instance.size());
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+
+    FT_ASSERT_EQ(3, deque_instance.front());
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    FT_ASSERT_EQ(7, deque_instance.back());
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+
+    int first_value = deque_instance.pop_front();
+    FT_ASSERT_EQ(3, first_value);
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+
+    int last_value = deque_instance.pop_back();
+    FT_ASSERT_EQ(7, last_value);
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+
+    FT_ASSERT_EQ(1UL, deque_instance.size());
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    FT_ASSERT_EQ(5, deque_instance.front());
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+
+    deque_instance.clear();
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    FT_ASSERT(deque_instance.empty());
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    return (1);
+}
+
+FT_TEST(test_ft_deque_pop_on_empty_sets_error, "ft_deque reports underflow errors when popping from an empty container")
+{
+    ft_deque<int> deque_instance;
+
+    int value_from_front = deque_instance.pop_front();
+    FT_ASSERT_EQ(0, value_from_front);
+    FT_ASSERT_EQ(DEQUE_EMPTY, deque_instance.get_error());
+
+    int value_from_back = deque_instance.pop_back();
+    FT_ASSERT_EQ(0, value_from_back);
+    FT_ASSERT_EQ(DEQUE_EMPTY, deque_instance.get_error());
+
+    FT_ASSERT(deque_instance.empty());
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    return (1);
+}
+
+FT_TEST(test_ft_deque_allocation_failure_sets_error, "ft_deque surfaces allocation failures during push operations")
+{
+    ft_deque<int> deque_instance;
+
+    cma_set_alloc_limit(1);
+    deque_instance.push_back(42);
+    cma_set_alloc_limit(0);
+
+    FT_ASSERT_EQ(DEQUE_ALLOC_FAIL, deque_instance.get_error());
+    FT_ASSERT(deque_instance.empty());
+
+    deque_instance.push_back(5);
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    FT_ASSERT_EQ(1UL, deque_instance.size());
+    FT_ASSERT_EQ(ER_SUCCESS, deque_instance.get_error());
+    return (1);
+}
+
+FT_TEST(test_ft_deque_move_transfers_ownership, "ft_deque move operations transfer nodes and reset sources")
+{
+    ft_deque<int> source_deque;
+    source_deque.push_back(1);
+    source_deque.push_back(2);
+    source_deque.push_back(3);
+    FT_ASSERT_EQ(ER_SUCCESS, source_deque.get_error());
+
+    ft_deque<int> moved_deque(ft_move(source_deque));
+
+    FT_ASSERT(source_deque.empty());
+    FT_ASSERT_EQ(ER_SUCCESS, source_deque.get_error());
+    FT_ASSERT_EQ(3UL, moved_deque.size());
+    FT_ASSERT_EQ(ER_SUCCESS, moved_deque.get_error());
+    FT_ASSERT_EQ(1, moved_deque.front());
+    FT_ASSERT_EQ(ER_SUCCESS, moved_deque.get_error());
+    FT_ASSERT_EQ(3, moved_deque.back());
+    FT_ASSERT_EQ(ER_SUCCESS, moved_deque.get_error());
+
+    ft_deque<int> target_deque;
+    target_deque.push_back(99);
+    FT_ASSERT_EQ(ER_SUCCESS, target_deque.get_error());
+
+    target_deque = ft_move(moved_deque);
+
+    FT_ASSERT(moved_deque.empty());
+    FT_ASSERT_EQ(ER_SUCCESS, moved_deque.get_error());
+    FT_ASSERT_EQ(3UL, target_deque.size());
+    FT_ASSERT_EQ(ER_SUCCESS, target_deque.get_error());
+    FT_ASSERT_EQ(1, target_deque.pop_front());
+    FT_ASSERT_EQ(ER_SUCCESS, target_deque.get_error());
+    FT_ASSERT_EQ(3, target_deque.pop_back());
+    FT_ASSERT_EQ(ER_SUCCESS, target_deque.get_error());
+    FT_ASSERT_EQ(1UL, target_deque.size());
+    FT_ASSERT_EQ(ER_SUCCESS, target_deque.get_error());
+    FT_ASSERT_EQ(2, target_deque.front());
+    FT_ASSERT_EQ(ER_SUCCESS, target_deque.get_error());
+    return (1);
+}
