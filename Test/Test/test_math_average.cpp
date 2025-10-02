9bd36ea9ff7a61d1c45313316b85d4414c579b70
Adyem <136982734+Adyem@users.noreply.github.com>
2025-10-02 11:53:06 +0200

    Add coverage for additional math utilities

---
+++ Test/Test/test_math_average.cpp
@@ -0,0 +1,42 @@
+#include "../../Math/math.hpp"
+#include "../../System_utils/test_runner.hpp"
+
+FT_TEST(test_math_average_even_ints, "math_average handles even integers")
+{
+    int result;
+
+    result = math_average(10, 4);
+    FT_ASSERT_EQ(7, result);
+    return (1);
+}
+
+FT_TEST(test_math_average_odd_ints_truncate, "math_average truncates fractional part toward zero")
+{
+    int result;
+
+    result = math_average(7, 8);
+    FT_ASSERT_EQ(7, result);
+    result = math_average(-5, 4);
+    FT_ASSERT_EQ(0, result);
+    return (1);
+}
+
+FT_TEST(test_math_average_long_values, "math_average works for long inputs")
+{
+    long result;
+
+    result = math_average(2147483646L, 2147483647L);
+    FT_ASSERT_EQ(2147483646L, result);
+    result = math_average(-2147483647L, -2147483648L);
+    FT_ASSERT_EQ(-2147483647L, result);
+    return (1);
+}
+
+FT_TEST(test_math_average_double_values, "math_average averages doubles")
+{
+    double result;
+
+    result = math_average(5.0, -3.0);
+    FT_ASSERT_EQ(1.0, result);
+    return (1);
+}
---
+++ Test/Test/test_math_swap.cpp
@@ -0,0 +1,28 @@
+#include "../../Math/math.hpp"
+#include "../../CPP_class/class_nullptr.hpp"
+#include "../../System_utils/test_runner.hpp"
+
+FT_TEST(test_math_swap_exchanges_values, "math_swap exchanges pointed integers")
+{
+    int first_number;
+    int second_number;
+
+    first_number = 3;
+    second_number = 9;
+    math_swap(&first_number, &second_number);
+    FT_ASSERT_EQ(9, first_number);
+    FT_ASSERT_EQ(3, second_number);
+    return (1);
+}
+
+FT_TEST(test_math_swap_null_pointer_safe, "math_swap ignores null pointers")
+{
+    int value;
+
+    value = 11;
+    math_swap(static_cast<int *>(ft_nullptr), &value);
+    FT_ASSERT_EQ(11, value);
+    math_swap(&value, static_cast<int *>(ft_nullptr));
+    FT_ASSERT_EQ(11, value);
+    return (1);
+}
