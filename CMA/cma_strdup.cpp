436593bcc2eb45798e9abb0934fe6e534df0c3ef
Adyem <136982734+Adyem@users.noreply.github.com>
2025-10-04 15:25:27 +0200

    Use ft_size_t in cma_strdup

--- CMA/cma_strdup.cpp
+++ CMA/cma_strdup.cpp
@@ -5,27 +5,29 @@
 
 char    *cma_strdup(const char *string)
 {
-    size_t  length;
-    char    *new_string;
-    size_t  index;
-    size_t  allocation_size;
+    size_t      measured_length_raw;
+    ft_size_t   length;
+    char        *new_string;
+    ft_size_t   index;
+    ft_size_t   allocation_size;
 
     if (!string)
         return (ft_nullptr);
     ft_errno = ER_SUCCESS;
-    length = ft_strlen_size_t(string);
+    measured_length_raw = ft_strlen_size_t(string);
     if (ft_errno != ER_SUCCESS)
     {
         ft_errno = FT_ERANGE;
         return (ft_nullptr);
     }
-    if (length > SIZE_MAX - 1)
+    if (measured_length_raw >= static_cast<size_t>(FT_SYSTEM_SIZE_MAX))
     {
         ft_errno = FT_ERANGE;
         return (ft_nullptr);
     }
-    allocation_size = length + 1;
-    new_string = static_cast<char *>(cma_malloc(static_cast<ft_size_t>(allocation_size)));
+    length = static_cast<ft_size_t>(measured_length_raw);
+    allocation_size = length + static_cast<ft_size_t>(1);
+    new_string = static_cast<char *>(cma_malloc(allocation_size));
     if (!new_string)
         return (ft_nullptr);
     index = 0;
