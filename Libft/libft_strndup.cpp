6495c3d6051c11f2eefe9e795f6227fcf7776fcd
Adyem <136982734+Adyem@users.noreply.github.com>
2025-10-08 20:44:30 +0200

    Add span-friendly Libft adapters

---
+++ Libft/libft_strndup.cpp
@@ -0,0 +1,33 @@
+#include "libft.hpp"
+#include "../CMA/CMA.hpp"
+#include "../CPP_class/class_nullptr.hpp"
+#include "../Errno/errno.hpp"
+
+char *ft_strndup(const char *string, size_t maximum_length)
+{
+    size_t copy_length;
+    char *duplicate;
+    size_t index;
+
+    ft_errno = ER_SUCCESS;
+    if (string == ft_nullptr)
+    {
+        ft_errno = FT_EINVAL;
+        return (ft_nullptr);
+    }
+    copy_length = ft_strnlen(string, maximum_length);
+    duplicate = static_cast<char *>(cma_malloc(copy_length + 1));
+    if (duplicate == ft_nullptr)
+    {
+        ft_errno = FT_EALLOC;
+        return (ft_nullptr);
+    }
+    index = 0;
+    while (index < copy_length)
+    {
+        duplicate[index] = string[index];
+        index++;
+    }
+    duplicate[copy_length] = '\0';
+    return (duplicate);
+}
---
+++ Libft/libft_strnlen.cpp
@@ -0,0 +1,21 @@
+#include "libft.hpp"
+#include "../CPP_class/class_nullptr.hpp"
+#include "../Errno/errno.hpp"
+
+size_t ft_strnlen(const char *string, size_t maximum_length)
+{
+    size_t index;
+
+    ft_errno = ER_SUCCESS;
+    if (string == ft_nullptr)
+    {
+        ft_errno = FT_EINVAL;
+        return (0);
+    }
+    index = 0;
+    while (index < maximum_length && string[index] != '\0')
+    {
+        index++;
+    }
+    return (index);
+}
