1e59fbdc96a5b9ae8bd6bfc830cd0678a0f0bf60
Adyem <136982734+Adyem@users.noreply.github.com>
2025-10-20 16:03:15 +0200

    Add tests for new time zone helpers

---
+++ Test/Test/test_time_timezone.cpp
@@ -0,0 +1,76 @@
+#include "../../Time/time.hpp"
+#include "../../Errno/errno.hpp"
+#include "../../System_utils/test_runner.hpp"
+#include <cstddef>
+#include <ctime>
+
+FT_TEST(test_time_format_iso8601_with_offset_zero, "time_format_iso8601_with_offset formats UTC time with zero offset")
+{
+    ft_string expected_string("1970-01-01T00:00:00+00:00");
+    ft_string result_string;
+
+    ft_errno = FT_ERR_OUT_OF_RANGE;
+    result_string = time_format_iso8601_with_offset(0, 0);
+    FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
+    FT_ASSERT_EQ(expected_string, result_string);
+    return (1);
+}
+
+FT_TEST(test_time_format_iso8601_with_offset_positive, "time_format_iso8601_with_offset formats positive offset")
+{
+    ft_string expected_string("1970-01-01T05:30:00+05:30");
+    ft_string result_string;
+
+    ft_errno = FT_ERR_OUT_OF_RANGE;
+    result_string = time_format_iso8601_with_offset(0, 330);
+    FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
+    FT_ASSERT_EQ(expected_string, result_string);
+    return (1);
+}
+
+FT_TEST(test_time_format_iso8601_with_offset_negative, "time_format_iso8601_with_offset formats negative offset")
+{
+    ft_string expected_string("1969-12-31T16:00:00-08:00");
+    ft_string result_string;
+
+    ft_errno = ER_SUCCESS;
+    result_string = time_format_iso8601_with_offset(0, -480);
+    FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
+    FT_ASSERT_EQ(expected_string, result_string);
+    return (1);
+}
+
+FT_TEST(test_time_convert_timezone_success, "time_convert_timezone converts between offsets")
+{
+    t_time converted_time;
+    bool conversion_result;
+
+    ft_errno = FT_ERR_INVALID_ARGUMENT;
+    conversion_result = time_convert_timezone(3600, 120, -300, &converted_time);
+    FT_ASSERT(conversion_result);
+    FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
+    FT_ASSERT_EQ(static_cast<t_time>(-21600), converted_time);
+    return (1);
+}
+
+FT_TEST(test_time_convert_timezone_invalid_argument, "time_convert_timezone validates output pointer")
+{
+    bool conversion_result;
+
+    ft_errno = ER_SUCCESS;
+    conversion_result = time_convert_timezone(0, 0, 0, NULL);
+    FT_ASSERT_EQ(false, conversion_result);
+    FT_ASSERT_EQ(FT_ERR_INVALID_ARGUMENT, ft_errno);
+    return (1);
+}
+
+FT_TEST(test_time_get_local_offset_requires_output, "time_get_local_offset requires at least one output pointer")
+{
+    bool offset_result;
+
+    ft_errno = ER_SUCCESS;
+    offset_result = time_get_local_offset(0, NULL, NULL);
+    FT_ASSERT_EQ(false, offset_result);
+    FT_ASSERT_EQ(FT_ERR_INVALID_ARGUMENT, ft_errno);
+    return (1);
+}
--- Time/time_format.cpp
+++ Time/time_format.cpp
@@ -4,7 +4,10 @@
 #include "../Errno/errno.hpp"
 #include "../Libft/libft.hpp"
 #include "../PThread/mutex.hpp"
+#include "../PThread/pthread.hpp"
 #include <ctime>
+#include <cstdio>
+#include <limits>
 
 static ft_string time_format_failure(int error_code)
 {
@@ -51,6 +54,73 @@ ft_string    time_format_iso8601(t_time time_value)
         return (time_format_failure(ft_errno));
     }
     formatted = ft_string(buffer);
+    ft_errno = ER_SUCCESS;
+    return (formatted);
+}
+
+ft_string    time_format_iso8601_with_offset(t_time time_value, int offset_minutes)
+{
+    std::tm *time_pointer;
+    std::tm time_storage;
+    char buffer[20];
+    char offset_buffer[7];
+    ft_string formatted;
+    static pt_mutex g_gmtime_mutex;
+    int mutex_result;
+    size_t strftime_result;
+    long long offset_seconds;
+    long long adjusted_epoch;
+    std::time_t adjusted_time;
+    char sign_character;
+    int absolute_offset;
+    int offset_hours;
+    int offset_minutes_part;
+    int snprintf_result;
+
+    offset_seconds = static_cast<long long>(offset_minutes) * 60LL;
+    adjusted_epoch = static_cast<long long>(time_value) + offset_seconds;
+    if (adjusted_epoch > static_cast<long long>(std::numeric_limits<std::time_t>::max()))
+        return (time_format_failure(FT_ERR_OUT_OF_RANGE));
+    if (adjusted_epoch < static_cast<long long>(std::numeric_limits<std::time_t>::min()))
+        return (time_format_failure(FT_ERR_OUT_OF_RANGE));
+    adjusted_time = static_cast<std::time_t>(adjusted_epoch);
+    mutex_result = g_gmtime_mutex.lock(THREAD_ID);
+    if (mutex_result != FT_SUCCESS)
+        return (time_format_failure(g_gmtime_mutex.get_error()));
+    time_pointer = std::gmtime(&adjusted_time);
+    if (!time_pointer)
+    {
+        mutex_result = g_gmtime_mutex.unlock(THREAD_ID);
+        if (mutex_result != FT_SUCCESS)
+            return (time_format_failure(g_gmtime_mutex.get_error()));
+        return (time_format_failure(FT_ERR_INVALID_ARGUMENT));
+    }
+    time_storage = *time_pointer;
+    mutex_result = g_gmtime_mutex.unlock(THREAD_ID);
+    if (mutex_result != FT_SUCCESS)
+        return (time_format_failure(g_gmtime_mutex.get_error()));
+    strftime_result = std::strftime(buffer, sizeof(buffer), "%Y-%m-%dT%H:%M:%S", &time_storage);
+    if (strftime_result == 0)
+    {
+        if (ft_errno == ER_SUCCESS)
+            return (time_format_failure(FT_ERR_INVALID_ARGUMENT));
+        return (time_format_failure(ft_errno));
+    }
+    if (offset_minutes >= 0)
+        sign_character = '+';
+    else
+        sign_character = '-';
+    absolute_offset = offset_minutes;
+    if (absolute_offset < 0)
+        absolute_offset = -absolute_offset;
+    offset_hours = absolute_offset / 60;
+    offset_minutes_part = absolute_offset % 60;
+    snprintf_result = std::snprintf(offset_buffer, sizeof(offset_buffer), "%c%02d:%02d",
+            sign_character, offset_hours, offset_minutes_part);
+    if (snprintf_result < 0 || snprintf_result >= static_cast<int>(sizeof(offset_buffer)))
+        return (time_format_failure(FT_ERR_INTERNAL));
+    formatted = ft_string(buffer);
+    formatted += offset_buffer;
     ft_errno = ER_SUCCESS;
     return (formatted);
 }
