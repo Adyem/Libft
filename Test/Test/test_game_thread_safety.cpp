1b1bee0f1fac98f851946ff27f31f8e7c8e80829
Adyem <136982734+Adyem@users.noreply.github.com>
2025-10-23 19:20:43 +0200

    Add thread safety tests for ft_reputation

--- Test/Test/test_game_thread_safety.cpp
+++ Test/Test/test_game_thread_safety.cpp
@@ -3,6 +3,7 @@
 #include "../../Game/game_debuff.hpp"
 #include "../../Game/game_equipment.hpp"
 #include "../../Game/game_experience_table.hpp"
+#include "../../Game/game_reputation.hpp"
 #include "../../Game/game_item.hpp"
 #include "../../Game/game_skill.hpp"
 #include "../../Game/game_upgrade.hpp"
@@ -173,6 +174,91 @@ static void run_upgrade_assignment_task(ft_upgrade *destination,
     {
         *destination = *source;
         if (destination->get_error() != ER_SUCCESS)
+        {
+            *failure_flag = 1;
+            return ;
+        }
+        index++;
+    }
+    *failure_flag = 0;
+    return ;
+}
+
+static void run_reputation_add_total_task(ft_reputation *reputation,
+        int iterations, int *failure_flag)
+{
+    int index;
+
+    index = 0;
+    while (index < iterations)
+    {
+        reputation->add_total_rep(1);
+        if (reputation->get_error() != ER_SUCCESS)
+        {
+            *failure_flag = 1;
+            return ;
+        }
+        index++;
+    }
+    *failure_flag = 0;
+    return ;
+}
+
+static void run_reputation_add_current_task(ft_reputation *reputation,
+        int iterations, int *failure_flag)
+{
+    int index;
+
+    index = 0;
+    while (index < iterations)
+    {
+        reputation->add_current_rep(1);
+        if (reputation->get_error() != ER_SUCCESS)
+        {
+            *failure_flag = 1;
+            return ;
+        }
+        index++;
+    }
+    *failure_flag = 0;
+    return ;
+}
+
+static void run_reputation_assignment_task(ft_reputation *destination,
+        ft_reputation *source, int iterations, int *failure_flag)
+{
+    int index;
+
+    index = 0;
+    while (index < iterations)
+    {
+        *destination = *source;
+        if (destination->get_error() != ER_SUCCESS)
+        {
+            *failure_flag = 1;
+            return ;
+        }
+        if (source->get_error() != ER_SUCCESS)
+        {
+            *failure_flag = 1;
+            return ;
+        }
+        index++;
+    }
+    *failure_flag = 0;
+    return ;
+}
+
+static void run_reputation_set_milestones_task(ft_reputation *reputation,
+        int id_base, int iterations, int *failure_flag)
+{
+    int index;
+
+    index = 0;
+    while (index < iterations)
+    {
+        reputation->set_milestone(id_base + index, index);
+        if (reputation->get_error() != ER_SUCCESS)
         {
             *failure_flag = 1;
             return ;
@@ -917,6 +1003,197 @@ FT_TEST(test_game_experience_table_cross_assignment_deadlock_free,
     }
     FT_ASSERT_EQ(ER_SUCCESS, left_table.get_error());
     FT_ASSERT_EQ(ER_SUCCESS, right_table.get_error());
+    FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
+    return (1);
+}
+
+FT_TEST(test_game_reputation_concurrent_rep_updates,
+        "ft_reputation total and current reputation updates remain consistent under concurrency")
+{
+    ft_reputation reputation;
+    ft_thread total_thread_one;
+    ft_thread total_thread_two;
+    ft_thread current_thread_one;
+    ft_thread current_thread_two;
+    int total_iterations;
+    int current_iterations;
+    int total_failure_one;
+    int total_failure_two;
+    int current_failure_one;
+    int current_failure_two;
+    int expected_total;
+
+    total_iterations = 512;
+    current_iterations = 256;
+    total_failure_one = -1;
+    total_failure_two = -1;
+    current_failure_one = -1;
+    current_failure_two = -1;
+    ft_errno = ER_SUCCESS;
+    reputation.set_total_rep(0);
+    reputation.set_current_rep(0);
+    FT_ASSERT_EQ(ER_SUCCESS, reputation.get_error());
+    total_thread_one = ft_thread(run_reputation_add_total_task, &reputation,
+            total_iterations, &total_failure_one);
+    FT_ASSERT_EQ(ER_SUCCESS, total_thread_one.get_error());
+    total_thread_two = ft_thread(run_reputation_add_total_task, &reputation,
+            total_iterations, &total_failure_two);
+    FT_ASSERT_EQ(ER_SUCCESS, total_thread_two.get_error());
+    current_thread_one = ft_thread(run_reputation_add_current_task, &reputation,
+            current_iterations, &current_failure_one);
+    FT_ASSERT_EQ(ER_SUCCESS, current_thread_one.get_error());
+    current_thread_two = ft_thread(run_reputation_add_current_task, &reputation,
+            current_iterations, &current_failure_two);
+    FT_ASSERT_EQ(ER_SUCCESS, current_thread_two.get_error());
+    total_thread_one.join();
+    FT_ASSERT_EQ(ER_SUCCESS, total_thread_one.get_error());
+    total_thread_two.join();
+    FT_ASSERT_EQ(ER_SUCCESS, total_thread_two.get_error());
+    current_thread_one.join();
+    FT_ASSERT_EQ(ER_SUCCESS, current_thread_one.get_error());
+    current_thread_two.join();
+    FT_ASSERT_EQ(ER_SUCCESS, current_thread_two.get_error());
+    FT_ASSERT_EQ(0, total_failure_one);
+    FT_ASSERT_EQ(0, total_failure_two);
+    FT_ASSERT_EQ(0, current_failure_one);
+    FT_ASSERT_EQ(0, current_failure_two);
+    expected_total = (total_iterations * 2) + (current_iterations * 2);
+    FT_ASSERT_EQ(expected_total, reputation.get_total_rep());
+    FT_ASSERT_EQ(ER_SUCCESS, reputation.get_error());
+    FT_ASSERT_EQ(current_iterations * 2, reputation.get_current_rep());
+    FT_ASSERT_EQ(ER_SUCCESS, reputation.get_error());
+    FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
+    return (1);
+}
+
+FT_TEST(test_game_reputation_cross_assignment_deadlock_free,
+        "ft_reputation assignment avoids deadlock and preserves mutex ordering")
+{
+    ft_reputation left_reputation;
+    ft_reputation right_reputation;
+    ft_thread left_thread;
+    ft_thread right_thread;
+    int iterations;
+    int left_failure_flag;
+    int right_failure_flag;
+
+    iterations = 256;
+    ft_errno = ER_SUCCESS;
+    left_failure_flag = -1;
+    right_failure_flag = -1;
+    left_reputation.set_total_rep(100);
+    left_reputation.set_current_rep(10);
+    left_reputation.set_milestone(1, 10);
+    left_reputation.set_milestone(2, 20);
+    left_reputation.set_rep(5, 500);
+    left_reputation.set_rep(6, 600);
+    FT_ASSERT_EQ(ER_SUCCESS, left_reputation.get_error());
+    right_reputation.set_total_rep(200);
+    right_reputation.set_current_rep(20);
+    right_reputation.set_milestone(3, 30);
+    right_reputation.set_milestone(4, 40);
+    right_reputation.set_rep(7, 700);
+    right_reputation.set_rep(8, 800);
+    FT_ASSERT_EQ(ER_SUCCESS, right_reputation.get_error());
+    left_thread = ft_thread(run_reputation_assignment_task, &left_reputation,
+            &right_reputation, iterations, &left_failure_flag);
+    FT_ASSERT_EQ(ER_SUCCESS, left_thread.get_error());
+    right_thread = ft_thread(run_reputation_assignment_task, &right_reputation,
+            &left_reputation, iterations, &right_failure_flag);
+    FT_ASSERT_EQ(ER_SUCCESS, right_thread.get_error());
+    left_thread.join();
+    FT_ASSERT_EQ(ER_SUCCESS, left_thread.get_error());
+    right_thread.join();
+    FT_ASSERT_EQ(ER_SUCCESS, right_thread.get_error());
+    FT_ASSERT_EQ(0, left_failure_flag);
+    FT_ASSERT_EQ(0, right_failure_flag);
+    FT_ASSERT_EQ(left_reputation.get_total_rep(), right_reputation.get_total_rep());
+    FT_ASSERT_EQ(ER_SUCCESS, left_reputation.get_error());
+    FT_ASSERT_EQ(ER_SUCCESS, right_reputation.get_error());
+    FT_ASSERT_EQ(left_reputation.get_current_rep(),
+            right_reputation.get_current_rep());
+    FT_ASSERT_EQ(ER_SUCCESS, left_reputation.get_error());
+    FT_ASSERT_EQ(ER_SUCCESS, right_reputation.get_error());
+    int milestone_keys[4];
+    int rep_keys[4];
+    int index;
+    int left_value;
+    int right_value;
+    int left_error;
+    int right_error;
+
+    milestone_keys[0] = 1;
+    milestone_keys[1] = 2;
+    milestone_keys[2] = 3;
+    milestone_keys[3] = 4;
+    index = 0;
+    while (index < 4)
+    {
+        left_value = left_reputation.get_milestone(milestone_keys[index]);
+        left_error = left_reputation.get_error();
+        right_value = right_reputation.get_milestone(milestone_keys[index]);
+        right_error = right_reputation.get_error();
+        FT_ASSERT_EQ(left_error, right_error);
+        if (left_error == ER_SUCCESS)
+            FT_ASSERT_EQ(left_value, right_value);
+        index++;
+    }
+    rep_keys[0] = 5;
+    rep_keys[1] = 6;
+    rep_keys[2] = 7;
+    rep_keys[3] = 8;
+    index = 0;
+    while (index < 4)
+    {
+        left_value = left_reputation.get_rep(rep_keys[index]);
+        left_error = left_reputation.get_error();
+        right_value = right_reputation.get_rep(rep_keys[index]);
+        right_error = right_reputation.get_error();
+        FT_ASSERT_EQ(left_error, right_error);
+        if (left_error == ER_SUCCESS)
+            FT_ASSERT_EQ(left_value, right_value);
+        index++;
+    }
+    FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
+    return (1);
+}
+
+FT_TEST(test_game_reputation_concurrent_milestone_updates,
+        "ft_reputation milestone updates remain thread safe under concurrency")
+{
+    ft_reputation reputation;
+    ft_thread thread_one;
+    ft_thread thread_two;
+    int iterations;
+    int failure_one;
+    int failure_two;
+    int index;
+
+    iterations = 64;
+    failure_one = -1;
+    failure_two = -1;
+    ft_errno = ER_SUCCESS;
+    thread_one = ft_thread(run_reputation_set_milestones_task, &reputation, 0,
+            iterations, &failure_one);
+    FT_ASSERT_EQ(ER_SUCCESS, thread_one.get_error());
+    thread_two = ft_thread(run_reputation_set_milestones_task, &reputation, 1000,
+            iterations, &failure_two);
+    FT_ASSERT_EQ(ER_SUCCESS, thread_two.get_error());
+    thread_one.join();
+    FT_ASSERT_EQ(ER_SUCCESS, thread_one.get_error());
+    thread_two.join();
+    FT_ASSERT_EQ(ER_SUCCESS, thread_two.get_error());
+    FT_ASSERT_EQ(0, failure_one);
+    FT_ASSERT_EQ(0, failure_two);
+    index = 0;
+    while (index < iterations)
+    {
+        FT_ASSERT_EQ(index, reputation.get_milestone(index));
+        FT_ASSERT_EQ(ER_SUCCESS, reputation.get_error());
+        FT_ASSERT_EQ(index, reputation.get_milestone(1000 + index));
+        FT_ASSERT_EQ(ER_SUCCESS, reputation.get_error());
+        index++;
+    }
     FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
     return (1);
 }
