f00c217990e5121703c4d400d737773c6c9ead58
Adyem <136982734+Adyem@users.noreply.github.com>
2025-12-04 15:59:40 +0100

    Adjust errno expectations in dialogue table tests

--- Test/Test/test_game_dialogue_table_getters_setters.cpp
+++ Test/Test/test_game_dialogue_table_getters_setters.cpp
@@ -26,7 +26,7 @@ FT_TEST(test_dialogue_table_register_line_sets_success, "register line stores en
     ft_dialogue_line fetched;
 
     build_dialogue_entries(line, script);
-    ft_errno = FT_ERR_TIMEOUT;
+    ft_errno = FT_ERR_INVALID_STATE;
     FT_ASSERT_EQ(ER_SUCCESS, table.register_line(line));
     FT_ASSERT_EQ(ER_SUCCESS, table.get_error());
     FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
@@ -43,7 +43,7 @@ FT_TEST(test_dialogue_table_register_script_sets_success, "register script store
     ft_dialogue_script fetched;
 
     build_dialogue_entries(line, script);
-    ft_errno = FT_ERR_TIMEOUT;
+    ft_errno = FT_ERR_INVALID_STATE;
     FT_ASSERT_EQ(ER_SUCCESS, table.register_script(script));
     FT_ASSERT_EQ(ER_SUCCESS, table.get_error());
     FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
@@ -88,7 +88,7 @@ FT_TEST(test_dialogue_table_set_lines_replaces_entries, "set_lines replaces stor
     table.register_line(line);
     table.fetch_line(99, fetched);
     lines.insert(10, ft_dialogue_line(10, ft_string("npc"), ft_string("hello"), ft_vector<int>()));
-    ft_errno = FT_ERR_TIMEOUT;
+    ft_errno = FT_ERR_INVALID_STATE;
     table.set_lines(lines);
     FT_ASSERT_EQ(ER_SUCCESS, table.get_error());
     FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
@@ -110,7 +110,7 @@ FT_TEST(test_dialogue_table_set_scripts_replaces_entries, "set_scripts swaps sto
     table.register_script(script);
     table.fetch_script(11, fetched);
     scripts.insert(11, ft_dialogue_script(11, ft_string("quest"), ft_string("desc"), 0, ft_vector<ft_dialogue_line>()));
-    ft_errno = FT_ERR_TIMEOUT;
+    ft_errno = FT_ERR_INVALID_STATE;
     table.set_scripts(scripts);
     FT_ASSERT_EQ(ER_SUCCESS, table.get_error());
     FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
@@ -129,7 +129,7 @@ FT_TEST(test_dialogue_table_get_lines_sets_success_errno, "get_lines sets succes
 
     build_dialogue_entries(line, script);
     lines.insert(7, line);
-    ft_errno = FT_ERR_TIMEOUT;
+    ft_errno = FT_ERR_INVALID_STATE;
     const ft_map<int, ft_dialogue_line> &const_lines = table.get_lines();
     FT_ASSERT_EQ(ER_SUCCESS, table.get_error());
     FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
@@ -145,7 +145,7 @@ FT_TEST(test_dialogue_table_get_scripts_sets_success_errno, "get_scripts sets su
 
     script = ft_dialogue_script(1, ft_string("intro"), ft_string("desc"), 0, ft_vector<ft_dialogue_line>());
     scripts.insert(1, script);
-    ft_errno = FT_ERR_TIMEOUT;
+    ft_errno = FT_ERR_INVALID_STATE;
     const ft_map<int, ft_dialogue_script> &const_scripts = table.get_scripts();
     FT_ASSERT_EQ(ER_SUCCESS, table.get_error());
     FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
@@ -158,7 +158,7 @@ FT_TEST(test_dialogue_table_get_error_str_success, "get_error_str returns succes
     ft_dialogue_table table;
     const char *message;
 
-    ft_errno = FT_ERR_TIMEOUT;
+    ft_errno = FT_ERR_INVALID_STATE;
     message = table.get_error_str();
     FT_ASSERT_EQ(ER_SUCCESS, ft_errno);
     FT_ASSERT(ft_strcmp(ft_strerror(ER_SUCCESS), message) == 0);
