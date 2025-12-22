c9253b8c0b8a48d30375f064815383875938272a
Adyem <136982734+Adyem@users.noreply.github.com>
2025-12-22 20:02:49 +0100

    Document recursive mutex support

---
+++ PThread/pthread_recursive_mutex.cpp
@@ -0,0 +1,220 @@
+#include "pthread.hpp"
+#include "recursive_mutex.hpp"
+#include "../Errno/errno.hpp"
+#include "../CMA/CMA.hpp"
+#include "../CPP_class/class_nullptr.hpp"
+#include "pthread_lock_tracking.hpp"
+
+pt_recursive_mutex::pt_recursive_mutex()
+    : _owner(0), _lock(false), _lock_depth(0), _error(FT_ERR_SUCCESSS),
+    _native_initialized(false), _state_mutex(ft_nullptr)
+{
+    ft_bzero(&this->_native_mutex, sizeof(pthread_mutex_t));
+    if (pthread_mutex_init(&this->_native_mutex, ft_nullptr) != 0)
+    {
+        this->_native_initialized = false;
+        this->set_error(FT_ERR_INVALID_STATE);
+        return ;
+    }
+    this->_native_initialized = true;
+    this->set_error(FT_ERR_SUCCESSS);
+    return ;
+}
+
+pt_recursive_mutex::~pt_recursive_mutex()
+{
+    if (this->_native_initialized)
+    {
+        pthread_mutex_destroy(&this->_native_mutex);
+        this->_native_initialized = false;
+    }
+    this->teardown_thread_safety();
+    return ;
+}
+
+void    pt_recursive_mutex::set_error(int error) const
+{
+    bool lock_acquired;
+
+    lock_acquired = false;
+    if (this->lock_internal(&lock_acquired) != 0)
+    {
+        ft_errno = error;
+        return ;
+    }
+    this->_error = error;
+    ft_errno = error;
+    this->unlock_internal(lock_acquired);
+    return ;
+}
+
+bool pt_recursive_mutex::ensure_native_mutex() const
+{
+    bool lock_acquired;
+    bool initialized;
+
+    lock_acquired = false;
+    if (this->lock_internal(&lock_acquired) != 0)
+        return (false);
+    initialized = this->_native_initialized;
+    if (initialized)
+    {
+        this->unlock_internal(lock_acquired);
+        return (true);
+    }
+    if (pthread_mutex_init(&this->_native_mutex, ft_nullptr) != 0)
+    {
+        this->_native_initialized = false;
+        this->unlock_internal(lock_acquired);
+        this->set_error(FT_ERR_INVALID_STATE);
+        return (false);
+    }
+    this->_native_initialized = true;
+    this->unlock_internal(lock_acquired);
+    return (true);
+}
+
+bool pt_recursive_mutex::lockState() const
+{
+    return (this->_lock.load(std::memory_order_acquire));
+}
+
+int pt_recursive_mutex::lock_internal(bool *lock_acquired) const
+{
+    if (lock_acquired != ft_nullptr)
+        *lock_acquired = false;
+    if (this->_state_mutex == ft_nullptr)
+    {
+        ft_errno = FT_ERR_SUCCESSS;
+        return (0);
+    }
+    this->_state_mutex->lock(THREAD_ID);
+    if (this->_state_mutex->get_error() != FT_ERR_SUCCESSS)
+    {
+        ft_errno = this->_state_mutex->get_error();
+        return (-1);
+    }
+    if (lock_acquired != ft_nullptr)
+        *lock_acquired = true;
+    ft_errno = FT_ERR_SUCCESSS;
+    return (0);
+}
+
+void pt_recursive_mutex::unlock_internal(bool lock_acquired) const
+{
+    if (!lock_acquired || this->_state_mutex == ft_nullptr)
+    {
+        ft_errno = FT_ERR_SUCCESSS;
+        return ;
+    }
+    this->_state_mutex->unlock(THREAD_ID);
+    if (this->_state_mutex->get_error() != FT_ERR_SUCCESSS)
+    {
+        ft_errno = this->_state_mutex->get_error();
+        return ;
+    }
+    ft_errno = FT_ERR_SUCCESSS;
+    return ;
+}
+
+void pt_recursive_mutex::teardown_thread_safety()
+{
+    if (this->_state_mutex != ft_nullptr)
+    {
+        this->_state_mutex->~pt_mutex();
+        cma_free(this->_state_mutex);
+        this->_state_mutex = ft_nullptr;
+    }
+    return ;
+}
+
+int pt_recursive_mutex::lock_state(bool *lock_acquired) const
+{
+    int result;
+
+    result = this->lock_internal(lock_acquired);
+    if (result != 0)
+        const_cast<pt_recursive_mutex *>(this)->set_error(ft_errno);
+    else
+        const_cast<pt_recursive_mutex *>(this)->set_error(FT_ERR_SUCCESSS);
+    return (result);
+}
+
+void pt_recursive_mutex::unlock_state(bool lock_acquired) const
+{
+    this->unlock_internal(lock_acquired);
+    if (this->_state_mutex != ft_nullptr && this->_state_mutex->get_error() != FT_ERR_SUCCESSS)
+        const_cast<pt_recursive_mutex *>(this)->set_error(this->_state_mutex->get_error());
+    else
+        const_cast<pt_recursive_mutex *>(this)->set_error(FT_ERR_SUCCESSS);
+    return ;
+}
+
+bool pt_recursive_mutex::is_owned_by_thread(pthread_t thread_id) const
+{
+    pthread_t owner_thread;
+    pt_mutex_vector owned_mutexes;
+    ft_size_t index;
+    bool lock_flag;
+
+    ft_errno = FT_ERR_SUCCESSS;
+    lock_flag = this->_lock.load(std::memory_order_acquire);
+    if (!lock_flag)
+    {
+        return (false);
+    }
+    owner_thread = this->_owner.load(std::memory_order_relaxed);
+    if (owner_thread != 0)
+    {
+        bool matches_owner;
+
+        matches_owner = (pt_thread_equal(owner_thread, thread_id) != 0);
+        ft_errno = FT_ERR_SUCCESSS;
+        return (matches_owner);
+    }
+    owned_mutexes = pt_lock_tracking::get_owned_mutexes(thread_id);
+    if (ft_errno != FT_ERR_SUCCESSS)
+        return (false);
+    index = 0;
+    while (index < owned_mutexes.size())
+    {
+        if (owned_mutexes[index] == &this->_native_mutex)
+        {
+            ft_errno = FT_ERR_SUCCESSS;
+            return (true);
+        }
+        index += 1;
+    }
+    ft_errno = FT_ERR_SUCCESSS;
+    return (false);
+}
+
+int pt_recursive_mutex::get_error() const
+{
+    bool lock_acquired;
+    int error_value;
+
+    lock_acquired = false;
+    if (this->lock_internal(&lock_acquired) != 0)
+        return (this->_error);
+    error_value = this->_error;
+    this->unlock_internal(lock_acquired);
+    return (error_value);
+}
+
+const char *pt_recursive_mutex::get_error_str() const
+{
+    int error_value;
+
+    error_value = this->get_error();
+    return (ft_strerror(error_value));
+}
+
+pthread_mutex_t   *pt_recursive_mutex::get_native_mutex() const
+{
+    if (!this->ensure_native_mutex())
+        return (ft_nullptr);
+    this->set_error(FT_ERR_SUCCESSS);
+    return (&this->_native_mutex);
+}
+
---
+++ PThread/pthread_recursive_timed_lock_mutex.cpp
@@ -0,0 +1,104 @@
+#include <errno.h>
+#include <vector>
+#include <time.h>
+#include "pthread.hpp"
+#include "recursive_mutex.hpp"
+#include "pthread_lock_tracking.hpp"
+#include "../Errno/errno.hpp"
+#include "../Libft/libft.hpp"
+
+static bool compute_absolute_deadline(const struct timespec &relative_time, struct timespec *absolute_time, int *error_code)
+{
+    struct timespec current_time;
+
+    if (relative_time.tv_sec < 0)
+    {
+        *error_code = FT_ERR_INVALID_ARGUMENT;
+        return (false);
+    }
+    if (relative_time.tv_nsec < 0)
+    {
+        *error_code = FT_ERR_INVALID_ARGUMENT;
+        return (false);
+    }
+    if (clock_gettime(CLOCK_REALTIME, &current_time) != 0)
+    {
+        *error_code = ft_map_system_error(errno);
+        return (false);
+    }
+    absolute_time->tv_sec = current_time.tv_sec + relative_time.tv_sec;
+    absolute_time->tv_nsec = current_time.tv_nsec + relative_time.tv_nsec;
+    while (absolute_time->tv_nsec >= 1000000000L)
+    {
+        absolute_time->tv_nsec -= 1000000000L;
+        absolute_time->tv_sec += 1;
+    }
+    *error_code = FT_ERR_SUCCESSS;
+    return (true);
+}
+
+int pt_recursive_mutex::try_lock_until(pthread_t thread_id, const struct timespec &absolute_time) const
+{
+    pt_mutex_vector owned_mutexes;
+    int mutex_error;
+
+    this->set_error(FT_ERR_SUCCESSS);
+    if (!this->ensure_native_mutex())
+        return (FT_SUCCESS);
+    if (this->_lock && pt_thread_equal(this->_owner.load(std::memory_order_relaxed), thread_id))
+    {
+        std::size_t current_depth;
+
+        current_depth = this->_lock_depth.load(std::memory_order_relaxed);
+        this->_lock_depth.store(current_depth + 1, std::memory_order_relaxed);
+        this->set_error(FT_ERR_SUCCESSS);
+        return (FT_SUCCESS);
+    }
+    owned_mutexes = pt_lock_tracking::get_owned_mutexes(thread_id);
+    if (ft_errno != FT_ERR_SUCCESSS)
+    {
+        this->set_error(ft_errno);
+        return (FT_SUCCESS);
+    }
+    if (!pt_lock_tracking::notify_wait(thread_id, &this->_native_mutex, owned_mutexes))
+    {
+        int tracker_error;
+
+        tracker_error = ft_errno;
+        pt_lock_tracking::notify_released(thread_id, &this->_native_mutex);
+        this->set_error(tracker_error);
+        return (FT_SUCCESS);
+    }
+    mutex_error = pthread_mutex_timedlock(&this->_native_mutex, &absolute_time);
+    if (mutex_error != 0)
+    {
+        pt_lock_tracking::notify_released(thread_id, &this->_native_mutex);
+        if (mutex_error == ETIMEDOUT)
+        {
+            this->set_error(FT_ERR_SUCCESSS);
+            return (ETIMEDOUT);
+        }
+        this->set_error(FT_ERR_INVALID_STATE);
+        return (FT_SUCCESS);
+    }
+    this->_owner.store(thread_id, std::memory_order_relaxed);
+    this->_lock = true;
+    this->_lock_depth.store(1, std::memory_order_relaxed);
+    pt_lock_tracking::notify_acquired(thread_id, &this->_native_mutex);
+    this->set_error(FT_ERR_SUCCESSS);
+    return (FT_SUCCESS);
+}
+
+int pt_recursive_mutex::try_lock_for(pthread_t thread_id, const struct timespec &relative_time) const
+{
+    struct timespec absolute_time;
+    int conversion_error;
+
+    if (!compute_absolute_deadline(relative_time, &absolute_time, &conversion_error))
+    {
+        this->set_error(conversion_error);
+        return (FT_SUCCESS);
+    }
+    return (this->try_lock_until(thread_id, absolute_time));
+}
+
